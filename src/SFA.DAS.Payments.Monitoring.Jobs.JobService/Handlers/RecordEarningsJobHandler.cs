0419452a3d6ee3b122ca6d4ae6a6f8722b505aaf
Ben Arnold <ben@arnoldfamily.me.uk>
2019-09-20 11:43:22 +0100

    Move monitoring handlers into JobService

---
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/RecordEarningsJobHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting.Client;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Monitoring.Jobs.JobService.Interfaces;
+using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.Monitoring.Jobs.JobsService.Handlers
+{
+    public class RecordEarningsJobHandler: BaseJobMessageHandler<RecordEarningsJob>
+    {
+        public RecordEarningsJobHandler(IServiceProxyFactory proxyFactory, IPaymentLogger logger): base(proxyFactory,logger)
+        {
+        }
+
+        protected override Task HandleMessage(RecordEarningsJob message, IMessageHandlerContext context, IJobService jobService,
+            CancellationToken cancellationToken)
+        {
+            return jobService.RecordEarningsJob(message, cancellationToken);
+        }
+    }
+}
