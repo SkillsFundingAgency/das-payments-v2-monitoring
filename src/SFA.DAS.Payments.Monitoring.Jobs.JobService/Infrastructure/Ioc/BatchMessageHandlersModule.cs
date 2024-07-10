6bd1d0ece60758b25eaa7a5fc185712c1c2cd4b6
Liam Wright <mail@liam-wright.co.uk>
2024-07-10 16:20:16 +0100

    PV2-3199 - Tests passing

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/BatchMessageHandlersModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/BatchMessageHandlersModule.cs
@@ -8,6 +8,7 @@ using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.PeriodEnd;
 using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.Submission;
 using SFA.DAS.Payments.Monitoring.Jobs.DataMessages.Commands;
 using SFA.DAS.Payments.ServiceFabric.Core;
+using NServiceBus;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
 {
@@ -75,6 +76,12 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
             builder.RegisterType<RecordPeriodEndFcsHandOverCompleteJobHandler>()
                 .As<IHandleMessageBatches<RecordPeriodEndFcsHandOverCompleteJob>>()
                 .InstancePerLifetimeScope();
+            builder.Register(c =>
+                {
+                    var appConfig = c.Resolve<IApplicationConfiguration>();
+                    return new EndpointConfiguration(appConfig.EndpointName);
+                }).As<EndpointConfiguration>()
+                .SingleInstance();
         }
     }
 }
--- src/SFA.DAS.Payments.Monitoring.Metrics.Data/MetricsPersistenceDataContext.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/MetricsPersistenceDataContext.cs
@@ -55,7 +55,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
             var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
             try
             {
-                await Database.ExecuteSqlCommandAsync($@"
+                await Database.ExecuteSqlRawAsync($@"
                     Delete 
                         From [Metrics].[SubmissionSummary] 
                     Where 
@@ -80,7 +80,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
             var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
             try
             {
-                await Database.ExecuteSqlCommandAsync($@"
+                await Database.ExecuteSqlRawAsync($@"
                     Delete 
                         From [Metrics].[PeriodEndSummary] 
                     Where 
@@ -91,7 +91,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
 
                 await PeriodEndSummaries.AddAsync(overallPeriodEndSummary, cancellationToken);
 
-                await Database.ExecuteSqlCommandAsync($@"
+                await Database.ExecuteSqlRawAsync($@"
                     Delete 
                         From [Metrics].[ProviderPeriodEndSummary] 
                     Where 
@@ -116,7 +116,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
             var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
             try
             {
-                await Database.ExecuteSqlCommandAsync($@"
+                await Database.ExecuteSqlRawAsync($@"
                     Delete 
                         From [Metrics].[SubmissionsSummary] 
                     Where 
