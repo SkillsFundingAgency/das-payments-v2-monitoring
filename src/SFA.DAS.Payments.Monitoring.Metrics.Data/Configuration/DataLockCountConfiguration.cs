2287875adff55970864ed93524ace2510aac5252
Liam Wright <mail@liam-wright.co.uk>
2024-07-12 13:15:07 +0100

    PV2-3199 - EF config and package updates

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/Configuration/DataLockCountConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static SFA.DAS.Payments.Monitoring.Metrics.Data.MetricsQueryDataContext;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Data.Configuration
+{
+    public class DataLockCountConfiguration : IEntityTypeConfiguration<DataLockCount>
+    {
+        public void Configure(EntityTypeBuilder<DataLockCount> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/Configuration/ProviderNegativeEarningsLearnerDataLockFundingLineTypeAmountsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Monitoring.Metrics.Model;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Data.Configuration
+{
+    public class ProviderNegativeEarningsLearnerDataLockFundingLineTypeAmountsConfiguration : IEntityTypeConfiguration<ProviderNegativeEarningsLearnerDataLockFundingLineTypeAmounts>
+    {
+        public void Configure(EntityTypeBuilder<ProviderNegativeEarningsLearnerDataLockFundingLineTypeAmounts> builder)
+        {
+            builder.HasNoKey();
+        }
+
+}
+}
