136dca14cc0d9fd65fc63729045bb633b56f8f50
David Read <david2.read@education.gov.uk>
2025-11-06 14:35:37 +0000

    PV2-3741 unit test for IoC registration of IJobMessageClientFactory

---
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Client.UnitTests/JobStatusClientModuleTests.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Monitoring.Jobs.Client.Infrastructure.Ioc;
+
+namespace SFA.DAS.Payments.Monitoring.Jobs.Client.UnitTests
+{
+    [TestFixture]
+    public class JobStatusClientModuleTests
+    {
+        [Test]
+        public void Should_register_IJobMessageClientFactory_As_Single_Instance()
+        {
+            // Arrange
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<JobStatusClientModule>();
+            var container = builder.Build();
+
+            // Act
+            var instance1 = container.Resolve<IJobMessageClientFactory>();
+            var instance2 = container.Resolve<IJobMessageClientFactory>();
+
+            // Assert
+            instance1.Should().BeOfType<JobMessageClientFactory>();
+            instance2.Should().BeSameAs(instance1);
+        }
+    }
+}
