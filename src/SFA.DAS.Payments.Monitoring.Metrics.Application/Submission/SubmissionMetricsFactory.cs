02fb51a826a5264c12828366baa63863c07173d0
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2026-01-30 15:02:25 +0000

    PV2-3876 - Wrapped SubmissionMetricsService into factory for spinning up individual contexts

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Application/Submission/SubmissionMetricsFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Autofac;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Application.Submission
+{
+    
+    public interface ISubmissionMetricsFactory
+    {
+        ISubmissionMetricsRepository Create();
+    }
+
+    public class SubmissionMetricsFactory : ISubmissionMetricsFactory
+    {
+        private readonly ILifetimeScope scope;
+
+        public SubmissionMetricsFactory(ILifetimeScope scope)
+        {
+            this.scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        }
+
+        public ISubmissionMetricsRepository Create()
+        {
+            return scope.Resolve<ISubmissionMetricsRepository>();
+        }
+    }
+}
