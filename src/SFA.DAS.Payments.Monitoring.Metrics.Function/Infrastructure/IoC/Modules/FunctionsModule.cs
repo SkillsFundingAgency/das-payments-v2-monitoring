b3e20908b3c8f2f3bed53fa63d2dc120a4d6d3ce
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2026-06-24 15:26:40 +0100

    PV2-4096 - Changed 2425 references to 2627 for annual year rollover

--- src/SFA.DAS.Payments.Monitoring.Metrics.Function/Infrastructure/IoC/Modules/FunctionsModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Function/Infrastructure/IoC/Modules/FunctionsModule.cs
@@ -77,12 +77,12 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Function.Infrastructure.IoC.Module
                     var configHelper = c.Resolve<IConfigurationHelper>();
 
                     var dbContextOptions = new DbContextOptionsBuilder().UseSqlServer(
-                        configHelper.GetConnectionString("DcEarnings2425ConnectionString"),
+                        configHelper.GetConnectionString("DcEarnings2627ConnectionString"),
                         optionsBuilder => optionsBuilder.CommandTimeout(270)).Options;
 
                     return new DcMetricsDataContext(dbContextOptions);
                 })
-                .Named<IDcMetricsDataContext>("DcEarnings2425DataContext")
+                .Named<IDcMetricsDataContext>("DcEarnings2627DataContext")
                 .InstancePerDependency();
 
             builder.Register((c, p) =>
