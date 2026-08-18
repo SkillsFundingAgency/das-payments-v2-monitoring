f615c1f0fb8b6920e9b67ab4dc2a35904616dc7e
David Read <david2.read@education.gov.uk>
2026-08-18 17:04:12 +0100

    Add error logging to PeriodEndRequestReportsClient

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndRequestReportsClient.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndRequestReportsClient.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Monitoring.Metrics.Model.PeriodEnd;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using SFA.DAS.Payments.Monitoring.Metrics.Model.PeriodEnd;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
 {
@@ -15,18 +16,25 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
     {
         private readonly string authCode;
         private readonly Uri functionAddressUri;
+        private readonly IPaymentLogger logger;
 
-        public PeriodEndRequestReportsClient(string authCode, string functionAddress)
+        public PeriodEndRequestReportsClient(string authCode, string functionAddress, IPaymentLogger logger)
         {
             this.authCode = authCode;
             functionAddressUri = new Uri(functionAddress);
+            this.logger = logger;
         }
 
         public async Task<bool> RequestReports(long jobId, short academicYear, byte collectionPeriod)
         {
             var result = await new HttpClient { Timeout = TimeSpan.FromSeconds(270) }.GetAsync(BuildUriFromParameters(jobId, academicYear, collectionPeriod));
 
-            if (!result.IsSuccessStatusCode) return false;
+            if (!result.IsSuccessStatusCode)
+            {
+                var response = await result.Content.ReadAsStringAsync();
+                logger.LogError($"Error retrieving period end reports, HTTP Status Code {result.StatusCode}, Response Message: {response}");
+                return false;
+            }
 
             var content = await result.Content.ReadAsStringAsync();
             var periodEndSummaryModel = JsonConvert.DeserializeObject<PeriodEndSummaryModel>(content);
